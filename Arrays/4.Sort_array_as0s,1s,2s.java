a70d59f30214b811a2e0c3acb0f18537a03b666d
Dhivya S <85786452+DhivyaRenuka@users.noreply.github.com>
2021-07-18 18:25:26 +0530

    Create 4.Sort_array_as0s,1s,2s.java

---
+++ Arrays/4.Sort_array_as0s,1s,2s.java
@@ -0,0 +1,32 @@
+class GFG {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob=new Solution();
+            ob.sort012(arr, n);
+            StringBuffer str = new StringBuffer();
+            for(int i=0; i<n; i++){
+                str.append(arr[i]+" ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+class Solution
+{
+    public static int [] sort012(int a[], int n)
+    {
+        // code here 
+        Arrays.sort(a);
+       return a;
+    }
+}
